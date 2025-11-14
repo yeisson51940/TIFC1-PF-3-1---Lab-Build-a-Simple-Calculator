9025ee708b080d809a318beb0b833e45a8725f0b
yeisson51940 <yeduardo1985@gmail.com>
2025-11-13 21:27:48 -0500

    Solucion Lab2

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ number2 = int(input("Ingrese el segundo numero: "))
 suma = number1 + number2
 print("La suma de",{number1},"y",{number2},"es:",{suma})
 
-Resultado = number2 - number1
-print("Resultado de la resta:", {Resultado})
+Resta = number2 - number1
+print("Resultado de la resta:", {Resta})
 
 multiplicacion = number1 * number2
 print("Resultado de la multiplicacion:", {multiplicacion})
@@ -15,3 +15,23 @@ print("Resultado de la division: ", {division})
 
 modulo = number1 % number2
 print("Resultado modulo: ", {modulo}) 
+
+number1 = int(input("Ingrese el primer numero: "))
+number2 = int(input("Ingrese el segundo numero: "))
+operacion = input("Introduce la operación (+, -, *, /): ")
+if operacion == '+':
+  resultado = number1 + number2
+elif operacion == '-':
+  resultado = number1 - number2
+elif operacion == '*':
+  resultado = number1 * number2
+elif operacion == '/':
+  if number2 != 0:
+    resultado = number1 / number2
+  else:
+    resultado = "No se puede dividir por cero"
+elif operacion ==  '%':
+    resultado = number1 % number2
+else:
+  resultado = "Operación no válida"
+print("El resultado es:", {resultado}) 
