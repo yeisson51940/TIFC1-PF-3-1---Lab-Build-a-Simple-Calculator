97415dd88fa598236b70035df931b75aa1adab15
yeisson51940 <yeduardo1985@gmail.com>
2025-11-13 20:50:22 -0500

    Solucion Lab2

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 number1 = int(input("Ingrese el primer numero: "))
 number2 = int(input("Ingrese el segundo numero: "))
+
 suma = number1 + number2
 print("La suma de",{number1},"y",{number2},"es:",{suma})
+
+Resultado = number2 - number1
+print("Resultado de la resta:", {Resultado})
+
+multiplicacion = number1 * number2
+print("Resultado de la multiplicacion:", {multiplicacion})
+
+division = number1 / number2
+print("Resultado de la division: ", {division})
+
+modulo = number1 % number2
+print("Resultado modulo: ", {modulo}) 
