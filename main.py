602a750e895a3abdea9ff65d26ae342367ea30af
yeisson51940 <yeduardo1985@gmail.com>
2025-11-13 19:22:21 -0500

    Solucion Lab2

---
+++ main.py
@@ -0,0 +1,4 @@
+number1 = int(input("Ingrese el primer numero: "))
+number2 = int(input("Ingrese el segundo numero: "))
+suma = number1 + number2
+print("La suma de",{number1},"y",{number2},"es:",{suma})
